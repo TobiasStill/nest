d2405ead2cfd14f877ee1131d077c7d913496755
Tobias Still <t.still@gmx.de>
2020-10-30 14:52:09 +0100

    Adjusted settings

--- src/settings/pink-orange.ts
+++ src/settings/pink-orange.ts
@@ -1,37 +1,43 @@
 import {Settings} from './settings';
 
 const settings: Settings = {
-  "background": "#000000",
-  "mesh": {
-    "castShadow": true,
-    "receiveShadow": true,
-    "color": "#BA55D3"
-  },
-  "ambient": {
-    "color": "#ffd700"
-  },
-  "spotlight": {
-    "color": "#ffd700",
-    "position": [
-      6000,
-      450,
-      450
-    ],
-    "castShadow": true,
-    "shadow_mapSize_width": 1024,
-    "shadow_mapSize_height": 1024,
-    "shadow_camera_near": 500,
-    "shadow_camera_far": 7000,
-    "shadow_camera_fov": 500
-  },
-  "pointlight": {
-    "color": "#ffd700",
-    "position": [
-      -100,
-      100,
-      200
-    ]
-  }
+    'background': '#000000',
+    'mesh': {
+        'castShadow': false,
+        'receiveShadow': true,
+        'color': '#ba55d3'
+    },
+    'ambient': {
+        'color': '#ffd700'
+    },
+    'spotlight': {
+        'color': '#fff832',
+        'position': [
+            6000,
+            450,
+            450
+        ],
+        'castShadow': true,
+        'shadow_mapSize_width': 1024,
+        'shadow_mapSize_height': 1024,
+        'shadow_camera_near': 500,
+        'shadow_camera_far': 7000,
+        'shadow_camera_fov': 500
+    },
+    'pointlight': {
+        'color': '#fd6a9b',
+        'position': [
+            -100,
+            100,
+            200
+        ],
+        'castShadow': true,
+        'shadow_mapSize_width': 1024,
+        'shadow_mapSize_height': 1024,
+        'shadow_camera_near': 500,
+        'shadow_camera_far': 7000,
+        'shadow_camera_fov': 500
+    }
 };
 
 export default settings;
